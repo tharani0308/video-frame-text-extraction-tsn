57e8faa093b1ccad9b80bd3acc33f8fccc6c3b07
Tharani R <104920843+tharani0308@users.noreply.github.com>
2025-05-02 23:29:13 +0530

    Create script.js

---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+// JavaScript can be added here if you want additional interactions, 
+// such as validating the input fields or providing visual feedback 
+// after submitting the form.
+console.log("JavaScript loaded.");
